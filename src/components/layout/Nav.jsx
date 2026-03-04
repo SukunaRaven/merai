01dfa9773d3815a01f748beb0c7a47f21dd1bd13
cctenshii <SissiWang2003@gmail.com>
2026-03-04 13:42:40 +0100

    Basic homepage layout is done, with a navbar layout!

---
+++ src/components/layout/Nav.jsx
@@ -0,0 +1,26 @@
+import {Link} from "react-router";
+import meraiLogo from '../../assets/TLE3-Mera-LogoIcon.png'
+
+function Nav() {
+    return (
+        <header className="bg-primary text-white-blue">
+            <div className="px-6">
+                <nav className="flex items-center justify-between h-20">
+                    <div className="flex items-center">
+                        <Link to="/">
+                            <img src={meraiLogo} alt="Merai Logo" className="h-20"/>
+                        </Link>
+                    </div>
+                    <div className="hidden md:flex items-center gap-6">
+                        <Link to="/profile"
+                              className="text-black-blue text-sm font-medium hover:text-white-blue transition-colors">Profiel</Link>
+                        <Link to="/settings"
+                              className="text-black-blue text-sm font-medium hover:text-white-blue transition-colors">Instellingen</Link>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Nav;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,38 @@
+import {Link} from "react-router";
+import Nav from "../components/layout/Nav.jsx";
+
 function Homepage() {
 
     return (
-        <main className="bg-white-blue">
-
-        </main>
+        <div className="bg-white-blue flex flex-col gap-3">
+            <Nav/>
+            <main className="py-15 px-25">
+                <div className="text-center">
+                    <h1 className="text-black-blue font-bold font-primary text-2xl">Welkom bij Merai!</h1>
+                    <p className="text-black-blue font-secondary">Merai is een educative tool die je op een speelse
+                        manier
+                        leert
+                        over je eigen AI gebruik doormiddel van
+                        mini games. </p>
+                </div>
+                <div className="p-4 flex gap-3 mt-auto">
+                    <Link to={`/profile`}
+                          className="flex-1 bg-blue text-white-blue text-center py-2 px-4 rounded-lg text-sm font-medium hover:bg-blue-dark hover:text-white-blue transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-blue-dark">
+                        Profiel
+                    </Link>
+                    <Link to={`/minigames`}
+                          className="flex-1 bg-blue text-white-blue text-center py-2 px-4 rounded-lg text-sm font-medium hover:bg-blue-dark hover:text-white-blue transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-blue-dark">
+                        Minigames
+                    </Link>
+                </div>
+                <div className="p-4 flex mt-auto">
+                    <Link to={`/attitudetest`}
+                          className="flex-1 bg-blue-light text-black-blue text-center py-10 px-4 rounded-lg text-sm font-medium font-secondary hover:bg-blue hover:text-white-blue transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-blue-dark">
+                        Klik hier om eerst met de 0 meting te gaan
+                    </Link>
+                </div>
+            </main>
+        </div>
     );
 }
 
