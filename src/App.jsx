e38b0735315477cf95c41e629368d7a41c82cb4f
cctenshii <SissiWang2003@gmail.com>
2026-03-09 09:41:58 +0100

    app.jsx comments gone

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import {createBrowserRouter, RouterProvider} from "react-router";
 import Homepage from "./pages/Homepage.jsx";
-// import AccessibilityPage from "./pages/AccessibilityPage.jsx"
-// import AiTipsAndTricksPage from "./pages/AiTipsAndTricksPage.jsx"
-// import AttitudeTestPage from "./pages/AttitudeTestPage.jsx"
+import AccessibilityPage from "./pages/AccessibilityPage.jsx"
+import AiTipsAndTricksPage from "./pages/AiTipsAndTricksPage.jsx"
+import AttitudeTestPage from "./pages/AttitudeTestPage.jsx"
 import DataManagement from "./pages/DataManagementPage.jsx"
 import FamilyPage from "./pages/FamilyPage.jsx"
 import MinigamesPage from "./pages/MinigamesPage.jsx";
-// import PrivacyPage from "./pages/PrivacyPage.jsx"
-// import ProfileAdjustPage from "./pages/ProfileAdjustPage.jsx"
-// import ProfilePage from "./pages/ProfilePage.jsx"
-// import SettingsPage from "./pages/SettingsPage.jsx"
+import PrivacyPage from "./pages/PrivacyPage.jsx"
+import ProfileAdjustPage from "./pages/ProfileAdjustPage.jsx"
+import ProfilePage from "./pages/ProfilePage.jsx"
+import SettingsPage from "./pages/SettingsPage.jsx"
 
 const router = createBrowserRouter([
     {
@@ -18,42 +18,42 @@ const router = createBrowserRouter([
                 path: '/',
                 element: <Homepage/>
             },
-            // {
-            //     path: '/attitudetest',
-            //     element: <AttitudeTestPage/>
-            // },
-            // {
-            //     path: '/profile',
-            //     element: <ProfilePage/>
-            // },
-            // {
-            //     path: '/settings',
-            //     element: <SettingsPage/>
-            // },
-            // {
-            //     path: '/settings/profileadjust',
-            //     element: <ProfileAdjustPage/>
-            // },
-            // {
-            //     path: '/settings/accessibility',
-            //     element: <AccessibilityPage/>
-            // },
+            {
+                path: '/attitudetest',
+                element: <AttitudeTestPage/>
+            },
+            {
+                path: '/profile',
+                element: <ProfilePage/>
+            },
+            {
+                path: '/settings',
+                element: <SettingsPage/>
+            },
+            {
+                path: '/settings/profileadjust',
+                element: <ProfileAdjustPage/>
+            },
+            {
+                path: '/settings/accessibility',
+                element: <AccessibilityPage/>
+            },
             {
                 path: '/settings/DataManagement',
                 element: <DataManagement/>
             },
-            // {
-            //     path: '/settings/privacy',
-            //     element: <PrivacyPage/>
-            // },
+            {
+                path: '/settings/privacy',
+                element: <PrivacyPage/>
+            },
             {
                 path: '/settings/family',
                 element: <FamilyPage/>
             },
-            // {
-            //     path: '/tipsandtricks',
-            //     element: <AiTipsAndTricksPage/>
-            // },
+            {
+                path: '/tipsandtricks',
+                element: <AiTipsAndTricksPage/>
+            },
             {
                 path: '/minigames',
                 element: <MinigamesPage/>
