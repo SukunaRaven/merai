fe1ca70d116cc91cae5a4fe70321a54d754e42b8
maudd <1091860@hr.nl>
2026-03-09 09:39:47 +0100

    Normal routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,68 @@
 import {createBrowserRouter, RouterProvider} from "react-router";
-// import Homepage from "./pages/Homepage.jsx";
+import Homepage from "./pages/Homepage.jsx";
 import AccessibilityPage from "./pages/AccessibilityPage.jsx"
-// import AiTipsAndTricksPage from "./pages/AiTipsAndTricksPage.jsx"
-// import AttitudeTestPage from "./pages/AttitudeTestPage.jsx"
-// import DataManagement from "./pages/DataManagementPage.jsx"
-// import FamilyPage from "./pages/FamilyPage.jsx"
-// import MinigamesPage from "./pages/MinigamesPage.jsx";
-// import PrivacyPage from "./pages/PrivacyPage.jsx"
-// import ProfileAdjustPage from "./pages/ProfileAdjustPage.jsx"
-// import ProfilePage from "./pages/ProfilePage.jsx"
+import AiTipsAndTricksPage from "./pages/AiTipsAndTricksPage.jsx"
+import AttitudeTestPage from "./pages/AttitudeTestPage.jsx"
+import DataManagement from "./pages/DataManagementPage.jsx"
+import FamilyPage from "./pages/FamilyPage.jsx"
+import MinigamesPage from "./pages/MinigamesPage.jsx";
+import PrivacyPage from "./pages/PrivacyPage.jsx"
+import ProfileAdjustPage from "./pages/ProfileAdjustPage.jsx"
+import ProfilePage from "./pages/ProfilePage.jsx"
 import SettingsPage from "./pages/SettingsPage.jsx"
-import {useEffect} from "react";
 
 const router = createBrowserRouter([
     {
         children: [
-            // {
-            //     path: '/',
-            //     element: <Homepage/>
-            // },
-            // {
-            //     path: '/attitudetest',
-            //     element: <AttitudeTestPage/>
-            // },
-            // {
-            //     path: '/profile',
-            //     element: <ProfilePage/>
-            // },
+            {
+                path: '/',
+                element: <Homepage/>
+            },
+            {
+                path: '/attitudetest',
+                element: <AttitudeTestPage/>
+            },
+            {
+                path: '/profile',
+                element: <ProfilePage/>
+            },
             {
                 path: '/settings',
                 element: <SettingsPage/>
             },
-            // {
-            //     path: '/settings/profileadjust',
-            //     element: <ProfileAdjustPage/>
-            // },
+            {
+                path: '/settings/profileadjust',
+                element: <ProfileAdjustPage/>
+            },
             {
                 path: '/settings/accessibility',
                 element: <AccessibilityPage/>
             },
-            // {
-            //     path: '/settings/DataManagement',
-            //     element: <DataManagement/>
-            // },
-            // {
-            //     path: '/settings/privacy',
-            //     element: <PrivacyPage/>
-            // },
-            // {
-            //     path: '/settings/family',
-            //     element: <FamilyPage/>
-            // },
-            // {
-            //     path: '/tipsandtricks',
-            //     element: <AiTipsAndTricksPage/>
-            // },
-            // {
-            //     path: '/minigames',
-            //     element: <MinigamesPage/>
-            // },
+            {
+                path: '/settings/DataManagement',
+                element: <DataManagement/>
+            },
+            {
+                path: '/settings/privacy',
+                element: <PrivacyPage/>
+            },
+            {
+                path: '/settings/family',
+                element: <FamilyPage/>
+            },
+            {
+                path: '/tipsandtricks',
+                element: <AiTipsAndTricksPage/>
+            },
+            {
+                path: '/minigames',
+                element: <MinigamesPage/>
+            },
         ]
     }
 ]);
 
 function App() {
-    useEffect(() => {
-        const savedSize = localStorage.getItem('user-font-size');
-
-        if (savedSize) {
-            document.documentElement.style.setProperty('--main-font-size', `${savedSize}px`);
-        }
-    }, []);
     return <RouterProvider router={router}/>;
 }
 
