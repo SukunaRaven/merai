113d09b97c02971b767127da4202a8070fa43898
maudd <1091860@hr.nl>
2026-03-11 10:25:45 +0100

    ADD gemini logo and small css fixes

--- src/pages/AiTipsAndTricksPage.jsx
+++ src/pages/AiTipsAndTricksPage.jsx
@@ -8,7 +8,9 @@ function AiTipsAndTricksPage() {
                 Tricks</h1>
             <main className="py-15 px-25">
                 <div className="flex justify-between font-secondary gap-10 ">
-                    <div className="bg-gray-400 py-10 px-20 mb-15 ml-26 rounded-xl ">LOGO van chatgpt, gemini, ofzo
+                    <div className="-mt-6 mb-15 ml-26 rounded-xlg">
+                        <img src='/gemini-logo.png' alt="Gemini Logo"/>
+                        <p className="italic text-sm text-center">Gemini</p>
                     </div>
                     <p className="text-lg text-center mt-10 mr-16">Blijf bij de bekende namen (zoals de officiële apps
                         van
@@ -21,23 +23,15 @@ function AiTipsAndTricksPage() {
                         abonnement. </p>
                 </div>
                 <div className="flex justify-between text-sm font-medium">
-                    <div className="text-center bg-white border-r-2 border-gray-200 rounded-l-xl p-7">
-                        <h2 className="text-black-blue font-bold font-primary text-2xl mb-5">AI tips voor
-                            tieners:</h2>
-                        <p className="text-black-blue mb-4">Worstel je met een lastig onderwerp voor
-                            school
-                            (zoals een natuurkundeles of een geschiedenisles)? Plak de tekst in een AI en vraag:
-                            "Leg
-                            dit
-                            concept uit in simpele taal voor een 5-jarige met een metafoor over gaming."</p>
-                        <p className="text-black-blue"> Upload een foto van een kledingstuk dat
-                            je
-                            al
-                            hebt en
-                            vraag de AI om 5
-                            verschillende outfits te bedenken met items die nu on-trend zijn.</p>
+                    <div className="flex-1 text-center bg-white  rounded-l-xl p-7">
+                        <h2 className="text-black-blue font-bold font-primary text-2xl mb-5">AI tips voor tieners:</h2>
+                        <p className="text-black-blue mb-4">Worstel je met een lastig onderwerp voor school (zoals een
+                            natuurkundeles of een geschiedenisles)? Plak de tekst in een AI en vraag: "Leg dit concept
+                            uit in simpele taal voor een 5-jarige met een metafoor over gaming."</p>
+                        <p className="text-black-blue"> Upload een foto van een kledingstuk dat je al hebt en vraag de
+                            AI om 5 verschillende outfits te bedenken met items die nu on-trend zijn.</p>
                     </div>
-                    <div className="text-center bg-white p-7 rounded-r-xl">
+                    <div className="flex-1 text-center bg-white border-l-2 border-gray-200 p-7 rounded-r-xl">
                         <h2 className="text-black-blue font-bold font-primary text-2xl mb-5">AI tips voor
                             ouders:</h2>
                         <p className="text-black-blue mb-4"> Maak een foto
