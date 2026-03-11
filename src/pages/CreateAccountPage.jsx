81947bbf15242d07156defb3dde8d9f0ca196c1e
cctenshii <SissiWang2003@gmail.com>
2026-03-11 13:32:09 +0100

    Small styling change

--- src/pages/CreateAccountPage.jsx
+++ src/pages/CreateAccountPage.jsx
@@ -47,7 +47,7 @@ function CreateAccountPage() {
     return (
         <div className="bg-white-blue flex flex-col gap-3 min-h-screen relative">
             <Nav/>
-            <main className="py-15 px-25">
+            <main className="py-15 px-50">
                 <form onSubmit={handleRegister}
                       className="flex flex-col gap-2 py-10 p-10 bg-white rounded-xl shadow-sm border border-gray-100">
                     <h1 className="text-black-blue font-bold font-primary text-2xl">Maak account aan</h1>
